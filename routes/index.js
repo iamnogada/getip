2efd3eee06895543dd72e02e05e20bd231568fb3
seungil <nogada@seungils-MacBook-Pro.local>
2020-11-26 18:25:55 +0900

    getip as text

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const ips = req.ip;
-  res.status(200).json(JSON.stringify(ips));
+  res.status(200).send(ips);
 });
 
 module.exports = router;
